426bc58672e9c6b9aa5dd642652988e689330c59
Airuop <uos1@yahoo.com>
2023-01-21 13:24:00 +0330

    Update speedtest2_yaml.sh

--- utils/speedtest/speedtest2_yaml.sh
+++ utils/speedtest/speedtest2_yaml.sh
@@ -1,5 +1,5 @@
 #准备好所Sprint
-wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.13.1/lite-linux-amd64-v0.13.1.gz
+wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
 gzip -d lite-linux-amd64.gz
 wget -O lite_config.json https://raw.githubusercontent.com/Airuop/cross/master/utils/speedtest/lite_config_yaml.json
 #运行 LiteSpeedTest
